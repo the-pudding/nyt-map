c19037eb5a8f2f610681593e54837dd2f2fd0b2f
russellgoldenberg <russellgoldenberg@gmail.com>
2018-12-11 16:03:58 -0500

    overhaul

--- src/js/graphic.js
+++ src/js/graphic.js
@@ -17,85 +17,18 @@ const MONTHS = [
 ];
 
 const FLAG_RATIO = 4 / 3;
+const MAX_WRAP = 320;
 
 let countryData = [];
 let monthData = [];
-// let alltimeData = [];
 
 const $timeline = d3.select('#timeline');
-// const $alltime = d3.select('#alltime');
-// const $ol = $alltime.select('ol');
 const $chart = $timeline.select('.figure__chart');
 const $annotation = $timeline.select('.figure__annotation');
 
 let flagW = 0;
 let flagH = 0;
-
-function resizeTimeline() {
-	const $year = $timeline.select('.year');
-	if ($year.size()) {
-		const timelineW = $timeline.node().offsetWidth;
-		const yearW = $year.node().offsetWidth;
-		const sideW = (timelineW - yearW) / 2;
-		const annotationW = timelineW - sideW;
-		$annotation.st('width', annotationW);
-
-		flagW = yearW / 12;
-		flagH = flagW / FLAG_RATIO;
-
-		$timeline.selectAll('.flag').st({ width: flagW, height: flagH });
-	}
-}
-
-function resizeAlltime() {
-	$ol.selectAll('li').st('height', flagH);
-}
-
-function resize() {
-	resizeTimeline();
-	resizeAlltime();
-}
-
-function setupTimeline() {
-	const nested = d3
-		.nest()
-		.key(d => d.year)
-		.entries(monthData);
-
-	const $year = $chart
-		.selectAll('.year')
-		.data(nested)
-		.enter()
-		.append('div.year');
-
-	$year.append('h3.title').text(d => d.key);
-	const $ul = $year.append('ul');
-
-	const $li = $ul
-		.selectAll('li')
-		.data(d => d.values)
-		.enter()
-		.append('li');
-
-	$li.append('span.month').text(d => MONTHS[+d.month - 1]);
-
-	$li.append('span.flag').html(d => {
-		const match = countryData.find(c => c.commonLower === d.country) || {
-			cca2: 'none'
-		};
-		const { cca2 } = match;
-		if (d.year === '1900' && d.month === '01') {
-			return `<img src="assets/flags/jpg-4x3-192-q70/${cca2.toLowerCase()}.jpg">
-			<img src="assets/flags/jpg-4x3-192-q70/jp.jpg">`;
-		}
-		return `<img src="assets/flags/jpg-4x3-192-q70/${cca2.toLowerCase()}.jpg">`;
-	});
-
-	$li.append('span.name').text(d => {
-		const match = countryData.find(c => c.commonLower === d.country);
-		return match ? match.common : d.country;
-	});
-}
+let wrapLength = 0;
 
 function duplicateConnector() {
 	const $a = d3.select(this);
@@ -111,7 +44,7 @@ function createAnnotation(data) {
 
 	const type = Annotate.annotationCustomType(Annotate.annotationCalloutCurve, {
 		className: 'custom',
-		connector: { type: 'curve', end: 'dot' },
+		connector: { type: 'curve', end: 'arrow' },
 		note: {
 			lineType: 'horizontal',
 			align: 'left'
@@ -123,7 +56,7 @@ function createAnnotation(data) {
 			title: d.year,
 			label: d.label,
 			padding: 0,
-			wrap: 110,
+			wrap: wrapLength,
 			bgPadding: { top: 5, left: 5, right: 5, bottom: 5 }
 		},
 		data: { yearOff: d.year - 1900, month: d.month },
@@ -157,18 +90,82 @@ function createAnnotation(data) {
 	// 	.st('opacity', 1);
 }
 
+function resizeTimeline() {
+	const $year = $timeline.select('.year');
+	if ($year.size()) {
+		const timelineW = $timeline.node().offsetWidth;
+		const yearW = $year.node().offsetWidth;
+		const sideW = (timelineW - yearW) / 2;
+		const annotationW = timelineW - sideW;
+		$annotation.st('width', annotationW);
+
+		flagW = yearW / 12;
+		flagH = flagW / FLAG_RATIO;
+
+		$timeline.selectAll('.flag').st({ width: flagW, height: flagH });
+
+		wrapLength = Math.min(sideW * 0.8, MAX_WRAP);
+
+		const test = d3
+			.range(100)
+			.map(() => 'm')
+			.join(' ');
+		const annoData = [
+			{
+				year: 1907,
+				month: 6,
+				label: test
+			}
+		];
+
+		createAnnotation(annoData);
+	}
+}
+
+function resize() {
+	resizeTimeline();
+}
+
+function setupTimeline() {
+	const nested = d3
+		.nest()
+		.key(d => d.year)
+		.entries(monthData);
+
+	const $year = $chart
+		.selectAll('.year')
+		.data(nested)
+		.enter()
+		.append('div.year');
+
+	$year.append('h3.title').text(d => d.key);
+	const $ul = $year.append('ul');
+
+	const $li = $ul
+		.selectAll('li')
+		.data(d => d.values)
+		.enter()
+		.append('li');
+
+	$li.append('span.month').text(d => MONTHS[+d.month - 1]);
+
+	$li.append('span.flag').html(d => {
+		const match = countryData.find(c => c.commonLower === d.country) || {
+			cca2: 'none'
+		};
+		const { cca2 } = match;
+
+		return `<img src="assets/flags/jpg-4x3-192-q70/${cca2.toLowerCase()}.jpg">`;
+	});
+
+	$li.append('span.name').text(d => {
+		const match = countryData.find(c => c.commonLower === d.country);
+		return match ? match.common : d.country;
+	});
+}
+
 function setupAnnotation() {
 	resizeTimeline();
-
-	const annoData = [
-		{
-			year: 1907,
-			month: 6,
-			label: 'Lemonade is released'
-		}
-	];
-
-	createAnnotation(annoData);
 }
 
 function cleanCountry(data) {
@@ -180,43 +177,6 @@ function cleanCountry(data) {
 		lng: +d.latlng.split(',')[1].trim()
 	}));
 }
-
-// function setupAlltime() {
-// 	const w = $ol.node().offsetWidth;
-// 	const $li = $ol
-// 		.selectAll('li')
-// 		.data(alltimeData.slice(0, 50))
-// 		.enter()
-// 		.append('li');
-// 	const scale = d3
-// 		.scaleLinear()
-// 		.domain([0, alltimeData[0].value])
-// 		.range([0, w]);
-// 	$li
-// 		.st('width', d => scale(d.value))
-// 		.st('background-image', d => {
-// 			const match = countryData.find(c => c.commonLower === d.key) || {
-// 				cca2: 'none'
-// 			};
-// 			const { cca2 } = match;
-// 			return `url("assets/flags/jpg-4x3-192-q70/${cca2.toLowerCase()}.jpg")`;
-// 		})
-// 		.st('background-size', `${flagW}px ${flagH}px`);
-// }
-
-// function cleanCountryData(data) {
-// 	const nested = d3
-// 		.nest()
-// 		.key(d => d.country)
-// 		.rollup(
-// 			values => d3.sum(values, v => +v.count) / d3.sum(values, v => +v.baseline)
-// 			// values => d3.sum(values, v => +v.count)
-// 		)
-// 		.entries(data);
-
-// 	nested.sort((a, b) => d3.descending(a.value, b.value));
-// 	return nested;
-// }
 
 function fixGaps(data) {
 	const [a, b] = d3.extent(data, d => +d.year);
@@ -239,17 +199,13 @@ function fixGaps(data) {
 }
 
 function loadResults() {
-	// const suffix = 'page-one';
-	// const suffix = 'all';
 	const files = ['month'];
 	const filepaths = files.map(d => `assets/data/result--${d}.csv`);
 	d3.loadData(...filepaths, (err, response) => {
 		if (err) console.log(err);
 		monthData = fixGaps(response[0].filter(d => +d.year < 2018));
-		// alltimeData = cleanCountryData(response[1]);
 		setupTimeline();
 		setupAnnotation();
-		// setupAlltime();
 		resize();
 	});
 }
